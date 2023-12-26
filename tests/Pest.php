66a970379e02d0beaad1abaab41d5c748f0063f1
Freek Van der Herten <freek@spatie.be>
2023-12-26 13:54:00 +0100

    wip

--- tests/Pest.php
+++ tests/Pest.php
@@ -34,7 +34,7 @@ function assertMatchesPdfSnapshot(string $pdfPath): void
 {
     $imagePath = getTempPath('test'.'.png');
 
-    $imagick = new Imagick($pdfPath);
+    $imagick = new Imagick($pdfPath . '[0]');
     $imagick->setImageFormat('png');
     file_put_contents($imagePath, $imagick);
 
