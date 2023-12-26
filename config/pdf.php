17a796099cf3d28d9a70310ce6fcb22401295eb8
Freek Van der Herten <freek@spatie.be>
2023-12-26 21:54:51 +0100

    wip

--- config/pdf.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for Spatie/LaravelPdf
-return [
-
-];
--- src/PdfServiceProvider.php
+++ src/PdfServiceProvider.php
@@ -4,15 +4,11 @@ namespace Spatie\LaravelPdf;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Spatie\LaravelPdf\Commands\LaravelPdfCommand;
 
 class PdfServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        $package
-            ->name('laravel-pdf')
-            ->hasConfigFile()
-            ->hasCommand(LaravelPdfCommand::class);
+        $package->name('laravel-pdf');
     }
 }
