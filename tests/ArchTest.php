ae9bcb0cd3d7e974b94aeff170b192fea23bcbd6
Ash Allen <mail@ashallendesign.co.uk>
2024-01-01 23:32:38 +0000

    Added architecture tests for facades and enums.

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Facade;
+
 arch('will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
+
+arch('Spatie\LaravelPdf\Enums namespace only contains enums')
+    ->expect('Spatie\LaravelPdf\Enums')
+    ->toBeEnums();
+
+arch('Spatie\LaravelPdf\Facades namespace only contains facades')
+    ->expect('Spatie\LaravelPdf\Facades')
+    ->toExtend(Facade::class);
