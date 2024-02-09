ab1cd44afbc6eea9d2c931126df2a706497f67a4
ArielMejiaDev <arielmejiadev@gmail.com>
2024-02-09 08:22:37 -0600

    add a blade directive for inlidedImage

--- src/PdfServiceProvider.php
+++ src/PdfServiceProvider.php
@@ -3,6 +3,9 @@
 namespace Spatie\LaravelPdf;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+use RuntimeException;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -26,5 +29,24 @@ class PdfServiceProvider extends PackageServiceProvider
         Blade::directive('totalPages', function () {
             return "<?php echo '<span class=\"totalPages\"></span>'; ?>";
         });
+
+        Blade::directive('inlidedImage', function ($url) {
+            $url = Str::of($url)->trim("'")->trim('"')->value();
+
+            if (!Str::of($url)->isUrl()) {
+                $imageContent = 'data:image/png;base64,' . base64_encode(file_get_contents(public_path($url)));
+                return "<?php echo '<img src=\"$imageContent\">'; ?>";
+            }
+
+            $response = Http::get($url);
+
+            if ($response->successful()) {
+                $imageContent = 'data:image/png;base64,' . base64_encode($response->body());
+                return "<?php echo '<img src=\"$imageContent\">'; ?>";
+            }
+
+            throw new RuntimeException('Failed to fetch the image');
+        });
+
     }
 }
